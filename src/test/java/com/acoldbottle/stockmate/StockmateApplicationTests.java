1715d30d6816b7f911eb4d16eaaf750b221ea390
kimbc715 <kimbc715@gmail.com>
2025-05-27 18:53:16 +0900

    init project

---
+++ src/test/java/com/acoldbottle/stockmate/StockmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acoldbottle.stockmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
