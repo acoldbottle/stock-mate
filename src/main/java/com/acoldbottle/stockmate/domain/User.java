6cb3a88d27db919b0bb08beb8e954835728e4331
kimbc715 <kimbc715@gmail.com>
2025-05-27 19:38:16 +0900

    Create User Entity

---
+++ src/main/java/com/acoldbottle/stockmate/domain/User.java
@@ -0,0 +1,30 @@
+package com.acoldbottle.stockmate.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false, unique = true)
+    private String userId;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Builder
+    public User(String userId, String password) {
+        this.userId = userId;
+        this.password = password;
+    }
+}
