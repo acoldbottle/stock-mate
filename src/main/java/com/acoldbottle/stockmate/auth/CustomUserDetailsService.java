c99359ff931e65989fbda247daf3a0048f898841
kimbc715 <kimbc715@gmail.com>
2025-05-27 20:52:35 +0900

    feat: CustomUserDetailsService

---
+++ src/main/java/com/acoldbottle/stockmate/auth/CustomUserDetailsService.java
@@ -0,0 +1,21 @@
+package com.acoldbottle.stockmate.auth;
+
+import com.acoldbottle.stockmate.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//        return userRepository.findByUsername
+    }
+}
