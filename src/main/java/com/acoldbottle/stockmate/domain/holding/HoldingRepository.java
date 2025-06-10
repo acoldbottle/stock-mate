a8e105b4fd6b0e859f4c829dd9bdafa922813d04
kimbc715 <kimbc715@gmail.com>
2025-06-10 18:36:56 +0900

    feat: Holding Jpa Repository

---
+++ src/main/java/com/acoldbottle/stockmate/domain/holding/HoldingRepository.java
@@ -0,0 +1,6 @@
+package com.acoldbottle.stockmate.domain.holding;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HoldingRepository extends JpaRepository<Holding, Long> {
+}
