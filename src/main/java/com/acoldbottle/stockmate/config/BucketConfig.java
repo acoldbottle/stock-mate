97bd806e83d4d8e572bbb3a88ce77ed3b4f69a44
kimbc715 <kimbc715@gmail.com>
2025-06-14 17:38:58 +0900

    feat: add BucketConfig for RateLimit

---
+++ src/main/java/com/acoldbottle/stockmate/config/BucketConfig.java
@@ -0,0 +1,23 @@
+package com.acoldbottle.stockmate.config;
+
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+
+@Configuration
+public class BucketConfig {
+
+    @Bean
+    public Bucket bucket() {
+        Refill refill = Refill.greedy(18, Duration.ofSeconds(1));
+        Bandwidth limit = Bandwidth.classic(18, refill);
+
+        return Bucket.builder()
+                .addLimit(limit)
+                .build();
+    }
+}
