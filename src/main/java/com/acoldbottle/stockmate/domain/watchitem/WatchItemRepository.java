24a7a9c8b0b9148ccfca964c037f3031d4fea5fc
kimbc715 <kimbc715@gmail.com>
2025-06-24 15:29:29 +0900

    feat: craete WatchItemRepository extends Jpa

---
+++ src/main/java/com/acoldbottle/stockmate/domain/watchitem/WatchItemRepository.java
@@ -0,0 +1,6 @@
+package com.acoldbottle.stockmate.domain.watchitem;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WatchItemRepository extends JpaRepository<WatchItem, Long> {
+}
