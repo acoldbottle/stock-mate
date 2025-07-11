516c6ee21645a414862347415cd0769632cf424e
kimbc715 <kimbc715@gmail.com>
2025-07-11 19:30:32 +0900

    feat(view): portfolio update ()

--- src/main/java/com/acoldbottle/stockmate/thymeleaf/PortfolioController.java
+++ src/main/java/com/acoldbottle/stockmate/thymeleaf/PortfolioController.java
@@ -2,11 +2,13 @@ package com.acoldbottle.stockmate.thymeleaf;
 
 import com.acoldbottle.stockmate.annotation.UserId;
 import com.acoldbottle.stockmate.api.portfolio.dto.req.PortfolioCreateReq;
+import com.acoldbottle.stockmate.api.portfolio.dto.req.PortfolioUpdateReq;
 import com.acoldbottle.stockmate.api.portfolio.dto.res.PortfolioGetRes;
 import com.acoldbottle.stockmate.api.portfolio.dto.res.PortfolioWithProfitRes;
 import com.acoldbottle.stockmate.api.portfolio.service.PortfolioService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@Slf4j
 @RequestMapping("/stockmate/portfolios")
 @RequiredArgsConstructor
 public class PortfolioController {
@@ -28,7 +31,6 @@ public class PortfolioController {
         model.addAttribute("portfolios", portfolioList);
         model.addAttribute("activePage", "portfolio");
         model.addAttribute("pageTitle", "StockMate - Portfolios");
-
         model.addAttribute("portfolioCreateReq", new PortfolioCreateReq());
         return "layout";
     }
@@ -40,11 +42,27 @@ public class PortfolioController {
             model.addAttribute(ERR_MSG, "포트폴리오 이름은 공백일 수 없습니다.");
             model.addAttribute("openCreateModal", true);
             model.addAttribute("portfolios", portfolioService.getPortfolioList(userId));
-            return "layout"; // 모달을 포함한 전체 페이지 다시 렌더링
+            return "layout";
         }
         portfolioService.createPortfolio(userId, portfolioCreateReq);
         return "redirect:/stockmate/portfolios";
     }
+
+    @PostMapping("/{portfolioId}/update")
+    public String updatePortfolio(@ModelAttribute @Valid PortfolioUpdateReq portfolioUpdateReq, BindingResult result,
+                                  Model model, @UserId Long userId, @PathVariable Long portfolioId) {
+        if (result.hasErrors()) {
+            model.addAttribute("portfolioErrId", portfolioId);
+            model.addAttribute(ERR_MSG, "포트폴리오 이름은 공백일 수 없습니다.");
+            model.addAttribute("openUpdateModal", true);
+            model.addAttribute("portfolioCreateReq", new PortfolioCreateReq());
+            model.addAttribute("portfolios", portfolioService.getPortfolioList(userId));
+            return "layout";
+        }
+        portfolioService.updatePortfolio(userId, portfolioId, portfolioUpdateReq);
+        return "redirect:/stockmate/portfolios";
+    }
+
 
     @PostMapping("/{portfolioId}/delete")
     public String deletePortfolio(@UserId Long userId, @PathVariable Long portfolioId) {
