223c2ac06efbb71b668985ab2b93b81fccac4a38
kimbc715 <kimbc715@gmail.com>
2025-07-12 21:18:09 +0900

    feat(view): search Stocks

--- src/main/java/com/acoldbottle/stockmate/thymeleaf/WatchlistController.java
+++ src/main/java/com/acoldbottle/stockmate/thymeleaf/WatchlistController.java
@@ -1,16 +1,16 @@
 package com.acoldbottle.stockmate.thymeleaf;
 
 import com.acoldbottle.stockmate.annotation.UserId;
+import com.acoldbottle.stockmate.api.stock.dto.res.StockSearchRes;
+import com.acoldbottle.stockmate.api.stock.service.StockService;
 import com.acoldbottle.stockmate.api.watchlist.dto.req.WatchItemCreateReq;
 import com.acoldbottle.stockmate.api.watchlist.dto.res.WatchItemGetRes;
 import com.acoldbottle.stockmate.api.watchlist.service.WatchlistService;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,6 +20,7 @@ import java.util.List;
 public class WatchlistController {
 
     private final WatchlistService watchlistService;
+    private final StockService stockService;
 
     @GetMapping
     public String getWatchlist(Model model, @UserId Long userId) {
@@ -35,5 +36,17 @@ public class WatchlistController {
     public String deleteWatchItem(@UserId Long userId, @PathVariable Long watchItemId) {
         watchlistService.deleteWatchItem(userId, watchItemId);
         return "redirect:/stockmate/watchlist";
+    }
+
+    @GetMapping("/search")
+    public String searchStocks(@RequestParam @NotBlank String keyword, @UserId Long userId,Model model) {
+        List<StockSearchRes> searchResults = stockService.searchByKeyword(keyword);
+        model.addAttribute("searchResults", searchResults);
+        List<WatchItemGetRes> watchlist = watchlistService.getWatchlist(userId);
+        model.addAttribute("watchlist", watchlist);
+        model.addAttribute("activePage", "watchlist");
+        model.addAttribute("pageTitle", "StockMate - Watchlist");
+        model.addAttribute("watchItemCreateReq", new WatchItemCreateReq());
+        return "layout";
     }
 }
