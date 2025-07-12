a3815ea846af227a79bdfbcba266e07f31d55dbc
kimbc715 <kimbc715@gmail.com>
2025-07-12 21:32:47 +0900

    rafactor: limit 10 when searchByKeyword

--- src/main/java/com/acoldbottle/stockmate/api/stock/service/StockService.java
+++ src/main/java/com/acoldbottle/stockmate/api/stock/service/StockService.java
@@ -21,6 +21,7 @@ public class StockService {
         List<Stock> searchedList = stockRepository.searchByKeyword(keyword);
         return searchedList.stream()
                 .map(StockSearchRes::from)
+                .limit(10)
                 .toList();
     }
 
