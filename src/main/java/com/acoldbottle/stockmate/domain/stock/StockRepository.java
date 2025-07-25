ab94510873077657190bd374612ce86e8c293499
kimbc715 <kimbc715@gmail.com>
2025-07-25 18:26:58 +0900

    feat: prioritize exact matches first in stock search results

--- src/main/java/com/acoldbottle/stockmate/domain/stock/StockRepository.java
+++ src/main/java/com/acoldbottle/stockmate/domain/stock/StockRepository.java
@@ -10,19 +10,22 @@ public interface StockRepository extends JpaRepository<Stock, String> {
 
     @Query(value =
     """
-    SELECT * FROM stocks
-    WHERE LOWER(symbol) LIKE LOWER(CONCAT(:keyword, '%'))
-       OR LOWER(eng_name) LIKE LOWER(CONCAT(:keyword, '%'))
-       OR LOWER(kor_name) LIKE LOWER(CONCAT(:keyword, '%'))
-    ORDER BY
-        CASE
-            WHEN LOWER(symbol) = LOWER(:keyword) THEN 0
-            WHEN LOWER(eng_name) = LOWER(:keyword) THEN 1
-            WHEN LOWER(kor_name) = LOWER(:keyword) THEN 1
-            ELSE 2
-        END,
-        symbol
-    LIMIT 10
+        SELECT * FROM stocks
+                WHERE is_delisted = false
+                  AND (
+                       LOWER(symbol) LIKE LOWER(CONCAT(:keyword, '%'))
+                       OR LOWER(eng_name) LIKE LOWER(CONCAT(:keyword, '%'))
+                       OR LOWER(kor_name) LIKE LOWER(CONCAT(:keyword, '%'))
+                  )
+                ORDER BY
+                    CASE
+                        WHEN LOWER(symbol) = LOWER(:keyword) THEN 0
+                        WHEN LOWER(eng_name) = LOWER(:keyword) THEN 1
+                        WHEN LOWER(kor_name) = LOWER(:keyword) THEN 1
+                        ELSE 2
+                    END,
+                    symbol
+                LIMIT 10
     """, nativeQuery = true)
     List<Stock> searchByKeyword(String keyword);
 }
