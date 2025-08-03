a8e1dfc670342c8e7788b0d842ba54116b3874ae
kimbc715 <kimbc715@gmail.com>
2025-08-03 21:12:45 +0900

    fix : holding -> holdings , tracked_symbol -> tracked_symbols In @Query

--- src/main/java/com/acoldbottle/stockmate/domain/trackedsymbol/TrackedSymbolRepository.java
+++ src/main/java/com/acoldbottle/stockmate/domain/trackedsymbol/TrackedSymbolRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.jpa.repository.Query;
 public interface TrackedSymbolRepository extends JpaRepository<TrackedSymbol, String> {
 
     @Modifying
-    @Query(value = "DELETE FROM tracked_symbol " +
+    @Query(value = "DELETE FROM tracked_symbols " +
             "WHERE symbol = :symbol " +
-            "  AND NOT EXISTS (SELECT 1 FROM holding WHERE symbol = :symbol) " +
+            "  AND NOT EXISTS (SELECT 1 FROM holdings WHERE symbol = :symbol) " +
             "  AND NOT EXISTS (SELECT 1 FROM watchlist WHERE symbol = :symbol)", nativeQuery = true)
     void deleteTrackedSymbolIfNotExists(String symbol);
 }
