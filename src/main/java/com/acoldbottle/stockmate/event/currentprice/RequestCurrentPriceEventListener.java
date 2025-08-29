37dc95206162ed71047455f5e2063d9d3934aa0f
kimbc715 <kimbc715@gmail.com>
2025-08-29 19:13:15 +0900

    refactor: add aysnc annotation

--- src/main/java/com/acoldbottle/stockmate/event/currentprice/RequestCurrentPriceEventListener.java
+++ src/main/java/com/acoldbottle/stockmate/event/currentprice/RequestCurrentPriceEventListener.java
@@ -1,31 +1,25 @@
 package com.acoldbottle.stockmate.event.currentprice;
 
 import com.acoldbottle.stockmate.api.currentprice.service.CurrentPriceService;
-import com.acoldbottle.stockmate.email.EmailService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
-import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class RequestCurrentPriceEventListener {
 
-    private final EmailService emailService;
     private final CurrentPriceService currentPriceService;
 
+    @Async
     @EventListener
     public void handleRequestCurrentPriceEvent(RequestCurrentPriceEvent event) {
         String symbol = event.getSymbol();
         String marketCode = event.getMarketCode();
-        CompletableFuture.runAsync(() -> currentPriceService.requestAndUpdateCurrentPrice(symbol, marketCode))
-                .exceptionally(e -> {
-                    log.error("[TrackedSymbolService] 저장 중에 오류 발생, symbol={}", symbol, e);
-                    emailService.sendEmailAlertError("[" + symbol + "] -> " + e.getCause().getMessage());
-                    return null;
-                });
+        currentPriceService.requestAndUpdateStock(symbol, marketCode);
     }
 }
