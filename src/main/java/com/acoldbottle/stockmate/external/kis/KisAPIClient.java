e2f338a2205fc4b99da6291572d937affd358d67
kimbc715 <kimbc715@gmail.com>
2025-08-29 19:05:19 +0900

    feat: add bucket for kis api rate limit

--- src/main/java/com/acoldbottle/stockmate/external/kis/KisAPIClient.java
+++ src/main/java/com/acoldbottle/stockmate/external/kis/KisAPIClient.java
@@ -2,12 +2,18 @@ package com.acoldbottle.stockmate.external.kis;
 
 import com.acoldbottle.stockmate.exception.ErrorCode;
 import com.acoldbottle.stockmate.exception.kis.KisCurrentPriceException;
+import com.acoldbottle.stockmate.exception.kis.KisTooManyRequestException;
 import com.acoldbottle.stockmate.external.kis.token.KisTokenService;
+import io.github.bucket4j.Bucket;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
+import java.time.Duration;
+
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class KisAPIClient {
@@ -24,23 +30,28 @@ public class KisAPIClient {
     private final RestClient restClient;
     private final KisTokenService kisTokenService;
 
-    public KisCurrentPriceRes requestCurrentPrice(String symbol, String marketCode) {
+    private final Bucket bucket;
+
+    public KisCurrentPriceRes requestCurrentPrice(String symbol, String marketCode) throws InterruptedException {
+
         String token = kisTokenService.getValidToken();
 
-        KisCurrentPriceRes res = restClient.get()
-                .uri("/uapi/overseas-price/v1/quotations/price?AUTH=y&EXCD=" + marketCode + "&SYMB=" + symbol)
-                .header("Authorization", token)
-                .header("appkey", key)
-                .header("appsecret", secret)
-                .header("tr_id", trId)
-                .header("custtype", custtype)
-                .retrieve()
-                .body(KisCurrentPriceRes.class);
-
-        if (res == null || !res.getRt_cd().equals("0")) {
-            throw new KisCurrentPriceException(ErrorCode.KIS_CURRENT_PRICE_ERROR);
+        if (bucket.asBlocking().tryConsume(1, Duration.ofSeconds(59))) {
+            KisCurrentPriceRes res = restClient.get()
+                    .uri("/uapi/overseas-price/v1/quotations/price?AUTH=y&EXCD=" + marketCode + "&SYMB=" + symbol)
+                    .header("Authorization", token)
+                    .header("appkey", key)
+                    .header("appsecret", secret)
+                    .header("tr_id", trId)
+                    .header("custtype", custtype)
+                    .retrieve()
+                    .body(KisCurrentPriceRes.class);
+            if (res == null || !res.getRt_cd().equals("0")) {
+                throw new KisCurrentPriceException(ErrorCode.KIS_CURRENT_PRICE_ERROR);
+            }
+            return res;
+        } else {
+            throw new KisTooManyRequestException(ErrorCode.KIS_TOO_MANY_REQUEST);
         }
-
-        return res;
     }
 }
