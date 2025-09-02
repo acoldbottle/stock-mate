a670dd663a8a28e1a817bc851bbdf1c16b29d50f
kimbc715 <kimbc715@gmail.com>
2025-09-02 15:22:29 +0900

    refactor: remove session management + add /.well-known in whitelist

--- src/main/java/com/acoldbottle/stockmate/config/SecurityConfig.java
+++ src/main/java/com/acoldbottle/stockmate/config/SecurityConfig.java
@@ -22,6 +22,7 @@ public class SecurityConfig {
 
     private static final String[] WHITE_LIST = {
             "/",
+            "/.well-known/**",
             "/api/login",
             "/api/signup",
             "/api/logout",
