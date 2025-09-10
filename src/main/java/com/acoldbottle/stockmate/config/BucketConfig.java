4aa8ada735f4522d04428314201527bb4814d6ea
kimbc715 <kimbc715@gmail.com>
2025-09-10 19:14:21 +0900

    fix: greedy(15, Duration.ofMillis(1000)) -> greedy(1, Duration.ofMillis(66))

--- src/main/java/com/acoldbottle/stockmate/config/BucketConfig.java
+++ src/main/java/com/acoldbottle/stockmate/config/BucketConfig.java
@@ -13,7 +13,7 @@ public class BucketConfig {
 
     @Bean
     public Bucket bucket() {
-        Refill refill = Refill.greedy(15, Duration.ofMillis(1000));
+        Refill refill = Refill.greedy(1, Duration.ofMillis(66));
         Bandwidth limit = Bandwidth.classic(15, refill);
 
         return Bucket.builder()
